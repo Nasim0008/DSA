7973ad6610ce464939d9c9ac2a429440a260525a
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-23 10:00:25 +0600

    Linear search for unique value in the array

---
+++ Array ,Searching and sorting/linear search (unique).cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,flag=0;
+    cout<< "Inter the size of array: ";
+    cin>>n;
+    int arr[n];
+    cout<< "Input the value of array: ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<< "Given array: ";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<< " ";
+    }
+    cout<<endl;
+    int value;
+    cout<< "Please,Inter the value you want to search: ";
+    cin>>value;
+
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==value)
+        {
+            cout<<i<<" th Index & "<<i+1<<" position"<<endl;
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
+    {
+        cout<<"Not Found"<<endl;
+    }
+
+
+}
+
